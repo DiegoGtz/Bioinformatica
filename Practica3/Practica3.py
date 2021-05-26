065d4ccaf520114294ab0b4ef002d9bf209879c8
Diego[GTZ] <dartz160@gmail.com>
2021-05-26 11:47:51 -0500

    Iniciar_Matriz

--- Practica3/Practica3.py
+++ Practica3/Practica3.py
@@ -1,1 +1,48 @@
 ############
+
+
+
+
+import numpy as np
+###########################
+
+######Practica 6 
+matSust=[[ 2,-7,-5,-7],
+         [-7, 2,-7,-5],
+         [-5,-7, 2,-7],
+         [-7,-5,-7, 2]]
+
+gapOpem=gapEXTEND= -5
+
+
+def getMaximo():
+	
+
+
+def inciar(s1,s2,gapOpem): 
+
+	M = np.zeros((len(s1)+1,len(s2)+1))
+
+	token  = 0 
+
+	for i in range(1,len(s2)+1):
+
+		token = token + gapOpem
+		M[0][i] = token
+		
+	token = 0 
+	for i in range(1,len(s1)+1):
+
+		token = token + gapOpem
+		M[i][0] = token
+		
+	print(M)
+
+
+
+
+S1 = "AGC" #columna
+S2 = "AAG" #Fila
+
+
+inciar(S1,S2,gapOpem)
