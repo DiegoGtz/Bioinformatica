1e5e28fd6bcdf4e84f4b9f36dce35e967259fc37
Diego[GTZ] <dartz160@gmail.com>
2021-05-26 23:05:59 -0500

    matriz

--- Practica 6/Practica6.1.py
+++ Practica 6/Practica6.1.py
@@ -19,10 +19,25 @@ def encontrar_valor(vec1, pos):
 	for i in range(len(vec1)+1):
 		if (vec1[i] == pos):
 			return i
-	
 
-def getMaximo():
-	pass
+M1 = np.zeros((4,4))
+M2 = []
+
+
+mi_dicc = dict()
+def getCamino(i,j,tipo):
+
+	M2.append((i,j,tipo))
+
+def getM():
+	return M2
+	
+def clear():
+	global M2
+	M2 = []
+
+
+
 
 
 def inciar(s1,s2,gapOpem): 
@@ -44,9 +59,9 @@ def inciar(s1,s2,gapOpem):
 		token = token + gapOpem
 		M[i][0] = token
 
-	print(M)
+	print "M = ", M
 
-	print(matSust)
+	print "S = ",matSust
 
 	v = []
 
@@ -56,13 +71,13 @@ def inciar(s1,s2,gapOpem):
 #			M[i][j]
 			pos_s1 = s1[j-1]
 			pos_s2 = s2[i-1]
-			print(pos_s1, pos_s2)
+		#	print(pos_s1, pos_s2)
 			
 			a = encontrar_valor(vec1, pos_s1)
 			b = encontrar_valor(vec2, pos_s2)
 
 		
-			print(v)
+			#print(v)
 			
 			f1 = M[i-1][j-1] + matSust[b][a]
 			f2 = M[i-1][j] + gapOpem
@@ -72,30 +87,39 @@ def inciar(s1,s2,gapOpem):
 			v.append(f2)	
 			v.append(f3)
 
+			#print("Valors max", v)
+			max1 = max(v)
+			
+			M[i][j] = max1
+
+			if f1 == max1 : 
+				getCamino(i-1,j-1,"d")
+			if f2 == max1 : 
+				getCamino(i-1,j,"T")
+			if f3 == max1 : 
+				getCamino(i,j-1,"L")
 
 			
+			mi_dicc[(i,j,)] = M2
 
-			print("Valors max", v)
-
-			M[i][j] = max(v)
-
-
-			print (a, b)
+			#print (a, b)
 			#print (matSust[b][a])
 			
 			print(f1, " " ,f2, " ", f3)
-			print("maximo", max(v))	
-			print("***********************************************")
-
-
+			#print("maximo", max(v))	
+			#print("***********************************************")
 
 			f1=f2=f3=0
 			v=[]
+		
+			clear()
 
 
 		
 	print("////////Resultado//////////")
-	print(M)	
+	print  M	
+
+
 
 S1 = "AAG" #Fila
 S2 = "AGC" #columna
