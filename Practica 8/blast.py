fde4c5fc0481adda0521a22b43740527c67bfb05
Nicoll del Rosario <rosamili45@gmail.com>
2021-06-21 19:58:17 -0500

    avance2

--- Practica 8/blast.py
+++ Practica 8/blast.py
@@ -109,6 +109,7 @@ def compara(lec,a):
 		for i in range (len(lec)-2):
 			if (cad[0]== lec[i] and cad[1]== lec[i+1] and cad[2]== lec[i+2]):
 				var+=1
+		print(lec.tell())
 
 		print (var, cad)
 		var=0
@@ -121,6 +122,20 @@ def archivos():
 		lec = lectura2(file)
 		compara(lec,a)
 
-archivos()
+#archivos()
+f=open("Q8BTM8.fasta", "r")
+
+linea=f.readline()
+print(len(linea))
+posi=f.tell()
+linea=f.readline()
+print(len(linea))
+while linea != "":
+	linea=linea[:-1]
+	print(posi,"  ",linea)
+	posi=f.tell()
+	linea=f.readline()
+
+
 
 
