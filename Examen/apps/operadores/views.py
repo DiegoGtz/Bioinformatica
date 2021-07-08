bee58612326a5c1d0f6cf82cb56d4cc63a065ade
Javo <rtacorac@unsa.edu.pe>
2021-07-08 09:04:20 -0500

    P1

--- Examen/apps/operadores/views.py
+++ Examen/apps/operadores/views.py
@@ -47,9 +47,207 @@ class Algoritmos2():
 
 	###########Pegarlo Aqui Javox#############
 
+	def encontrar_valor(self,vec1,pos):
+
+		for i in range(len(vec1)+1):
+			if (vec1[i] == pos):
+				return i
+
+	def verificar (self,M,i,j):
+		cont=0
+		if(M[i][j][0] == 1):
+			cont+=1
+		if(M[i][j][1] == 1):
+			cont+=1
+		if(M[i][j][2] == 1):
+			cont+=1
+		return cont
+	
+	def camino(self,M,i,j,ss1,ss2):
+		S1 = self.S1
+		S2 = self.S2
+		if (self.verificar (M,i,j) == 1):
+			if(M[i][j][0] == 1):
+				#diagonal
+				ss1 += S1[j-1]
+				ss2 += S2[i-1]
+				#print("diagonal",i-1," ",j-1)
+				if(i==0 or j==0):
+					print("Resultado: ")
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss2)
+					print(ss2)
+					return
+				self.camino(M,i-1,j-1,ss1,ss2)
+
+			elif(M[i][j][1] == 1):
+				#arriba
+				ss1 += "-"
+				ss2 += S2[i-1] 
+				#print("arriba",i-1," ",j)
+				if(i==0 or j==0):
+					print("Resultado: ")
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss1)
+					print(ss2)
+					return 
+				self.camino(M,i-1,j,ss1,ss2)
+
+			elif(M[i][j][2] == 1):
+				#costado
+				ss1 += S1[j-1] 
+				ss2 += "-"
+				#print("costado",i," ",j-1)
+				if(i==0 or j==0):
+					print("Resultado: ")
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss1)
+					print(ss2)
+					return
+				self.camino(M,i,j-1,ss1,ss2)
+
+		elif(self.verificar(M,i,j) == 2):
+			#print("ramificacion")
+			if(M[i][j][0] == 1):
+				#diagonal
+				ss1 += S1[j-1]
+				ss2 += S2[i-1]
+				#print("diagonal",i-1," ",j-1)
+				if(i==0 or j==0):
+					print("Resultado: ")
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss1)
+					print(ss2)
+					return
+				self.camino(M,i-1,j-1,ss1,ss2)
+
+			if(M[i][j][1] == 1):
+				#arriba
+				ss1 += "-"
+				ss2 += S2[i-1] 
+				#print("arriba",i-1," ",j)
+				if(i==0 or j==0):
+					print("Resultado: ")
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss1)
+					print(ss2)
+					return 
+				self.camino(M,i-1,j,ss1,ss2)
+
+			if(M[i][j][2] == 1):
+				#costado
+				ss1 += S1[j-1] 
+				ss2 += "-"
+				#print("costado",i," ",j-1)
+				if(i==0 or j==0):
+					print("Resultado: ")					
+					self.vec.append(ss1)
+					self.vec.append(ss2)
+					print(ss1)
+					print(ss2)
+					return
+				self.camino(M,i,j-1,ss1,ss2)
+
+		elif(self.verificar(M,i,j) == 0):
+			print("Resultado: ")
+			self.vec.append(ss1)
+			self.vec.append(ss2)
+			print(ss1)
+			print(ss2)
+
+	def inciar(self,s1,s2,gapOpem): 
+
+		pos_s1 = pos_s2 = ""
+		
+		M = np.zeros((len(s1)+1,len(s2)+1))
+		A = [0,0,0,0]
+		M_vacia = []
+		vector_vacio = []
+		
+		for i in range(len(s2)+1):
+			for j in range(len(s1)+1):
+				vector_vacio.append(A.copy())
+			M_vacia.append(vector_vacio)
+			vector_vacio = []
+
+		token  = 0
+		for i in range(1,len(s2)+1):
+			token = token + gapOpem
+			#print ("token ", token)
+			M_vacia[0][i][3] = token
+			M_vacia[0][i][2] = 1
+
+		token = 0
+		for i in range(1,len(s1)+1):
+			token = token + gapOpem
+			M_vacia[i][0][3] = token
+			M_vacia[i][0][1] = 1
+
+		#print ("M = ", M_vacia)
+		#print ("S = ",matSust)
+
+		v = []
+
+		for i in range(1, len(s2)+1):
+			for j in range(1, len(s1)+1):
 
 
+				pos_s1 = s1[j-1]
+				pos_s2 = s2[i-1]
+			#	print(pos_s1, pos_s2)
+				
+				#a = self.encontrar_valor(self.vec1, pos_s1)
+				#b = self.encontrar_valor(self.vec2, pos_s2)
 
+				value = self.identicalMatch
+				
+				if pos_s2 != pos_s1:
+					value = self.mismatch  
+			
+				#print(v)
+				
+				f1 = M_vacia[i-1][j-1][3] + value
+				f2 = M_vacia[i-1][j][3] + gapOpem
+				f3 = M_vacia[i][j-1][3] + gapOpem
+
+				v.append(f1)
+				v.append(f2)	
+				v.append(f3)
+
+				#print("Valors max", v)
+				max1 = max(v)
+				
+				M_vacia[i][j][3] = max1
+				
+				if f1 == max1 : 
+					M_vacia[i][j][0] = 1
+				if f2 == max1 : 
+					M_vacia[i][j][1] = 1
+				if f3 == max1 : 
+					M_vacia[i][j][2] = 1
+
+				f1=f2=f3=0
+				v=[]
+			
+		print("////////Resultado1//////////")
+		#print(M_vacia)
+		#imprimir(M_vacia)
+		print("***********************************************")
+		i=3
+		j=3
+		ss1 = ""
+		ss2 = ""
+
+		self.camino(M_vacia,i,j,ss1,ss2)
+	
+	def empezar(self):
+		self.inciar(self.S1,self.S2,self.gapOpem)
+		return self.vec
 
 	###############################################
 
